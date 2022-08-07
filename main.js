05e08034e3d8332b4dce207356ecc2e1daa2923c
UC947746 <947746@pdsb.net>
2022-08-07 13:51:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Navbar Javascript
+function toggleMenu (){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
